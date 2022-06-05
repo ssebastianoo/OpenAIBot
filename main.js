b327deb704e7c3663ad98c48937a273df38d6886
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-05 14:54:44 +0200

    init

---
+++ main.js
@@ -0,0 +1,34 @@
+const { Telegraf } = require('telegraf')
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = require('./config');
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: config.apiKey,
+}));
+const bot = new Telegraf(config.token)
+
+bot.command('ask', async (ctx) => {
+    const args = ctx.update.message.text.split(" ");
+    args.shift()
+    let question = args.join(" ");
+    if (question.length == 0) {
+        return ctx.telegram.sendAnimation(ctx.chat.id, 'https://c.tenor.com/b7QvDz0lDdUAAAAC/giopaddo-godo.gif', {reply_to_message_id: ctx.message.message_id})
+    }
+    const completion = await openai.createCompletion("text-davinci-001", {
+        prompt: question,
+    });
+    ctx.reply(completion.data.choices[0].text, {reply_to_message_id: ctx.message.message_id});
+})
+
+/* bot.command('ask', async (ctx) => {
+    const completion = await openai.createCompletion("text-davinci-001", {
+        prompt: match[1],
+    });
+    ctx.reply(completion.data.choices[0].text);
+}) */
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
