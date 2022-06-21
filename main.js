e3b622d0bcd36412dabab3421b83d034279c2096
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-21 19:07:31 +0200

    only works in goups

--- main.js
+++ main.js
@@ -1,51 +1,48 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require("telegraf");
 const { Configuration, OpenAIApi } = require("openai");
-const config = require('./config');
+const config = require("./config");
 
-const openai = new OpenAIApi(new Configuration({
+const openai = new OpenAIApi(
+  new Configuration({
     apiKey: config.apiKey,
-}));
-const bot = new Telegraf(config.token)
+  })
+);
+const bot = new Telegraf(config.token);
 
 bot.start((ctx) => {
-    ctx.reply("Hello, this is a bot that uses OpenAI.\nAsk anything using /ask followed by your question, if your directly texting the bot you don't need to use /ask, just ask your question.");
-})
-
-async function askAI(question) {
-    const completion = await openai.createCompletion("text-davinci-001", {
-        prompt: question,
-        max_tokens: 1000,
-    });
-    return completion;
-}
-
-bot.command('ask', async (ctx) => {
-    const args = ctx.update.message.text.split(" ");
-    args.shift()
-    let question = args.join(" ");
-    if (question.length == 0) {
-        return ctx.reply('Type something after /ask to ask me stuff.', {reply_to_message_id: ctx.message.message_id});
-    }
-    const completion = await askAI(question);
-    ctx.reply(completion.data.choices[0].text, {reply_to_message_id: ctx.message.message_id});
-})
-
-bot.on('message', async (ctx) => {
-    console.log(ctx.message.chat)
-    if (ctx.message.chat.type == 'private') {
-        const completion = await askAI(ctx.message.text);
-        ctx.reply(completion.data.choices[0].text, {reply_to_message_id: ctx.message.message_id});
-    }
+  ctx.reply(
+    "Hello, this is a bot that uses OpenAI.\nAsk anything using /ask followed by your question, if your directly texting the bot you don't need to use /ask, just ask your question."
+  );
 });
 
-/* bot.command('ask', async (ctx) => {
-    const completion = await openai.createCompletion("text-davinci-001", {
-        prompt: match[1],
+async function askAI(question) {
+  const completion = await openai.createCompletion("text-davinci-001", {
+    prompt: question,
+    max_tokens: 1000,
+  });
+  return completion.data.choices[0].text;
+}
+
+bot.command("ask", async (ctx) => {
+  if (ctx.message.chat.type == "private") {
+    return ctx.reply("This bot doesn't work in private messages and can't be added in groups, comma 22 https://en.wikipedia.org/wiki/Catch-22_(logic)")
+  }
+
+  const args = ctx.update.message.text.split(" ");
+  args.shift();
+  let question = args.join(" ");
+  if (question.length == 0) {
+    return ctx.reply("Type something after /ask to ask me stuff.", {
+      reply_to_message_id: ctx.message.message_id,
     });
-    ctx.reply(completion.data.choices[0].text);
-}) */
+  }
+  const completion = await askAI(question);
+  ctx.reply(completion, {
+    reply_to_message_id: ctx.message.message_id,
+  });
+});
 
-bot.launch()
+bot.launch();
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
